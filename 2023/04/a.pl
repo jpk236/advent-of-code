90a7bb5c3b66e3418b29b2422f3095862263a09b
Justin Kulikowski <jpk236@jpk236.com>
2023-12-04 07:21:22 -0500

    Solved

--- 2023/04/a.pl
+++ 2023/04/a.pl
@@ -9,16 +9,17 @@ $Data::Dumper::Sortkeys = 1;
 $Data::Dumper::Deepcopy = 1;
 open STDERR, '>&STDOUT';
 
-my $test = 1;
+my $test = 0;
 
 open (FILE, "input.txt")        if ($test == 0);
 open (FILE, "input_test.txt")   if ($test == 1);
 chomp(my @input = <FILE>);
 close (FILE);
 
-my %cards = ();
+my $total = 0;
+
 foreach my $line (@input) {
-    print "line: |" , $line , "|\n";
+    #print "line: |" , $line , "|\n";
 
     my ($card, $numbers) = split(/\s*:\s*/, $line);
 
@@ -29,8 +30,37 @@ foreach my $line (@input) {
     my @winningNum = split(/\s+/io, $winning);
     my @numWeHave = split(/\s+/io, $num);
 
-    foreach my $number (@numWeHave) {
+    my $match = 0;
+    my $subTotal = 0;
+    foreach my $win (@winningNum) {
+        foreach my $check (@numWeHave) {
+            #print "$win == $check?\n";
+
+            if ($win == $check) {
+                $match++;
+
+                if ($match == 1) {
+                    $subTotal += 1;
+                }
+                else {
+                    $subTotal *= 2;
+                }
+
+                last;
+            }
+        }
+
+        #print "$win subTotal: |" , $subTotal , "|\n";
+
+        #my $gak = <>;
     }
 
-    my $gak = <>;
+    print "card $cardNum subTotal: |" , $subTotal , "|\n";
+
+    #my $gak = <>;
+
+    $total += $subTotal;
 }
+
+print "total: |" , $total , "|\n";
+
