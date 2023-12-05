d116959b13a4f4a2cdd69087fc9ef7e0df806ef3
Justin Kulikowski <jpk236@jpk236.com>
2023-12-04 22:41:05 -0500

    Solved

--- 2023/04/b.pl
+++ 2023/04/b.pl
@@ -9,15 +9,79 @@ $Data::Dumper::Sortkeys = 1;
 $Data::Dumper::Deepcopy = 1;
 open STDERR, '>&STDOUT';
 
-my $test = 1;
+my $test = 0;
 
 open (FILE, "input.txt")        if ($test == 0);
 open (FILE, "input_test.txt")   if ($test == 1);
 chomp(my @input = <FILE>);
 close (FILE);
 
+my %cards = ();
 foreach my $line (@input) {
-    print "line: |" , $line , "|\n";
+    my ($card, $numbers) = split(/\s*:\s*/, $line);
 
-    my $gak = <>;
+    my ($cardNum, $winning, $num) = $line =~ /^Card\s*(\d+):([^|]+)\|([^\$]+)$/io;
+    $winning =~ s/^\s*|\s*$//gio;
+    $num =~ s/^\s*|\s*$//gio;
+
+    my @winningNum = split(/\s+/io, $winning);
+    my @numWeHave = split(/\s+/io, $num);
+
+    $cards{$cardNum} = {
+        'cardNum'       => $cardNum,
+        'winningNum'    => [@winningNum],
+        'numWeHave'     => [@numWeHave],
+        'copies'        => 1,
+    };
 }
+
+foreach my $card (sort { $a <=> $b } keys %cards) {
+    my $cardNum = $cards{$card}{'cardNum'};
+    my $copies = $cards{$card}{'copies'};
+    my @winningNum = @{$cards{$card}{'winningNum'}};
+    my @numWeHave = @{$cards{$card}{'numWeHave'}};
+
+    my $match = 0;
+    foreach my $win (@winningNum) {
+        foreach my $check (@numWeHave) {
+            if ($win == $check) {
+                $match++;
+            }
+        }
+    }
+
+    my $start = $card+1;
+    my $end = (($card+$match) > scalar @input) ? scalar @input : ($card+$match);
+
+    foreach my $range ($start..$end) {
+        foreach (1..$copies) {
+            $cards{$range}{'copies'}++;
+        }
+    }
+
+    print "cardNum($cardNum): match($match)\n\n";
+
+    foreach my $card (sort { $a <=> $b } keys %cards) {
+        my $cardNum = $cards{$card}{'cardNum'};
+        my $copies = $cards{$card}{'copies'};
+
+        print "cardNum($cardNum) : $copies\n";
+    }
+
+    print "\n";
+
+    #my $gak = <>;
+}
+
+my $total = 0;
+foreach my $card (sort { $a <=> $b } keys %cards) {
+    my $cardNum = $cards{$card}{'cardNum'};
+    my $copies = $cards{$card}{'copies'};
+
+    print "cardNum($cardNum) : $copies\n";
+
+    $total += $copies;
+}
+
+print "total: |" , $total , "|\n";
+
